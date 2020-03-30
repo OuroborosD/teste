0da61ff41eca672b8d9b49a262e48c3f8ec2d0b8
Diego S Olivieiragit <diego-siebra@hotmail.com>
2020-03-30 19:29:12 -0300

    vamos testar se essa é a solução

--- base/settings.py
+++ base/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'teste',
         'USER': 'teste',
-        'PASSWORD': 'teste!',
+        'PASSWORD': 'teste',
         'HOST':'localhost',
         'PORT': '5432'
     }
